cef9b30154e780aa8a46027ba8c6b4b64b89be19
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-21 08:44:23 -0300

    criar interface para informar as propriedades que o ButtonContainer possui

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface ButtonContainerProps {
+  variant: 'primary' | 'secondary' | 'success' | 'danger'
+}
+
 export const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
