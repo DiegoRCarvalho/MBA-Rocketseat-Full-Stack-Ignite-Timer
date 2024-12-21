f0d7abea8233d96ff2a688940d37390b85650590
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-21 08:17:59 -0300

    exportar uma constante que recebe um styled component estilizando um elemento button do HTML

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,20 +1,6 @@
-.button {
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
-}
-
-.primary {
-  background: rgb(59, 59, 219);
-}
-
-.secondary {
-  background: rgb(219, 150, 59);
-}
-
-.success {
-  background: rgb(219, 59, 59);
-}
-
-.danger {
-  background: rgb(2, 146, 45);
-}
+`
