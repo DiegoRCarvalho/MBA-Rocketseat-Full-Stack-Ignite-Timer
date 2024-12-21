17c4ff50d933e8988fe796404ad529077684dcdc
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-21 08:57:03 -0300

    criar um objeto para receber as propriedades condicionais

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,6 +6,13 @@ interface ButtonContainerProps {
   variant: ButtonVariant
 }
 
+const buttonVariants = {
+  primary: 'rgb(59, 59, 219)',
+  secondary: 'rgb(219, 150, 59)',
+  danger:'rgb(219, 59, 59)',
+  success: 'rgb(2, 146, 45)'
+}
+
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
