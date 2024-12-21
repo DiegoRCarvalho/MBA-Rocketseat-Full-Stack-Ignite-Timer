5f150d8bcace7b709ece645e1b52eaae76af4269
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-21 10:01:02 -0300

    definir que um conjunto de elementos irá utilizar um tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { deafultTheme } from "./styles/themes/default";
 import { Button } from "./components/Button";
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={deafultTheme}>
       <Button variant="primary" /> 
       <Button variant="secondary" /> 
       <Button variant="success" /> 
       <Button variant="danger" /> 
       <Button /> 
-    </>
+    </ThemeProvider >
   )
 }
