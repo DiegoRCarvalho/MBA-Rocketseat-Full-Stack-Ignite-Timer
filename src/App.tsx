03972f178164e3a698dffa1d4a98cdc0f45d2b73
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-21 18:11:44 -0300

    utilizar estilios globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
@@ -11,6 +12,9 @@ export function App() {
       <Button variant="success" /> 
       <Button variant="danger" /> 
       <Button /> 
+
+      {/* Para funcionar, o GlobalStyle tem que ficar dentro do ThemeProvider*/}
+      <GlobalStyle />
     </ThemeProvider >
   )
 }
