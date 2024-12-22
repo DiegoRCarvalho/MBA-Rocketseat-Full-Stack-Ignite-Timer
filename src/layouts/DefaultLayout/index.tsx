13f94afc621b8759c2f3a30f6de456cafaf540d1
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-22 12:00:52 -0300

    utilizando a estilização no container de DefaultLayout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       {/* A função Outlet do React Router é um componente usado para renderizar componentes filhos de rotas aninhadas. Ele atua como um marcador de posição no layout de uma rota pai, indicando onde o conteúdo das rotas filhas será inserido. */}
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
