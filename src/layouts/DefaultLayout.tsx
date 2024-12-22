01a8b62f0b8331531282f9156a028e7c2fbef7a2
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-22 07:39:15 -0300

    adicionar componentes filhos ao header com a função outlet do react-dom

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from 'react-router-dom'
 import { Header } from '../components/Header'
 
 export function DefaultLayout() {
   return (
     <div>
       <Header />
+      {/* A função Outlet do React Router é um componente usado para renderizar componentes filhos de rotas aninhadas. Ele atua como um marcador de posição no layout de uma rota pai, indicando onde o conteúdo das rotas filhas será inserido. */}
+      <Outlet />
     </div>
   )
 }
