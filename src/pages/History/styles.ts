f385b031472a37aa318e6b91d30e1d86baac06fe
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2024-12-25 11:18:36 -0300

    criar e estilizar o componente de status

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -59,3 +59,27 @@ export const HistoryList = styled.div`
     }
   }
 `
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const // Utilizado para informar ao Typescript que esse texto nunca irá mudar.
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS // O Typescript não consegue ler objetos Javascript ele consegue ler apenas a tipagem dos objetos.
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: ''; // Para que o elemento possa aparecer em tela precisa ter um conteúdo, mesmo que vazio.
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
